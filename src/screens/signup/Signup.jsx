99480b05493076063a6c705853ba9feaf0295604
priyanshu <priyanshuqpwp@gmail.com>
2023-10-13 21:11:08 +0530

    created env config. and tested signup form submit

--- src/screens/signup/Signup.jsx
+++ src/screens/signup/Signup.jsx
@@ -11,69 +11,71 @@ const Signup = () => {
         <div className={styles.formContainer}>
           <LogoComponent />
           <div className={styles.signupForm}>
-            <h2 className={styles.title}>Create Account</h2>{" "}
-            <div>
-              <label className={styles.label} for="name">
-                Your Name
-              </label>
-              <br />
-              <input
-                className={styles.input}
-                type="text"
-                id="name"
-                name="name"
-              />
-            </div>
-            <div>
-              <label className={styles.label} for="mobile">
-                Mobile number
-              </label>
-              <br />
-              <input
-                className={styles.input}
-                type="text"
-                id="mobile"
-                name="mobile"
-              />
-            </div>
-            <div>
-              <label className={styles.label} for="email">
-                Email Id
-              </label>
-              <br />
-              <input
-                className={styles.input}
-                type="email"
-                id="email"
-                name="email"
-              />
-            </div>
-            <div>
-              <label className={styles.label} for="password">
-                Password
-              </label>
-              <br />
-              <input
-                className={styles.input}
-                type="password"
-                id="password"
-                name="password"
-              />
-            </div>
-            <button className={styles.continueBtn} type="submit">
-              Continue
-            </button>
-            <p className={styles.terms}>
-              By continuing, you agree to Musicart privacy notice and conditions
-              of use.
-            </p>
+            <form action={`${process.env.REACT_APP_API_BASE_URL}/api/signup`} method="POST">
+              <h2 className={styles.title}>Create Account</h2>{" "}
+              <div>
+                <label className={styles.label} for="name">
+                  Your Name
+                </label>
+                <br />
+                <input
+                  className={styles.input}
+                  type="text"
+                  id="name"
+                  name="name"
+                />
+              </div>
+              <div>
+                <label className={styles.label} for="mobile">
+                  Mobile number
+                </label>
+                <br />
+                <input
+                  className={styles.input}
+                  type="text"
+                  id="mobile"
+                  name="mobile"
+                />
+              </div>
+              <div>
+                <label className={styles.label} for="email">
+                  Email Id
+                </label>
+                <br />
+                <input
+                  className={styles.input}
+                  type="email"
+                  id="email"
+                  name="email"
+                />
+              </div>
+              <div>
+                <label className={styles.label} for="password">
+                  Password
+                </label>
+                <br />
+                <input
+                  className={styles.input}
+                  type="password"
+                  id="password"
+                  name="password"
+                />
+              </div>
+              <button className={styles.continueBtn} type="submit">
+                Continue
+              </button>
+              <p className={styles.terms}>
+                By continuing, you agree to Musicart privacy notice and
+                conditions of use.
+              </p>
+            </form>
           </div>
 
           <div className={styles.loginLink}>
             Already have an account?{" "}
             <Link
               to="/login"
-              style={{ textDecoration: "underline", color: "inherit"}}
+              style={{ textDecoration: "underline", color: "inherit" }}
             >
               Log In
             </Link>
